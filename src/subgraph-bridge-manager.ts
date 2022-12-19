38b89d711cb0f1084519c40f837ae1babc983fd5
mercuricchloride <mercuryprotocol@protonmail.com>
2022-12-19 16:11:25 -0700

    merkle root working

--- src/subgraph-bridge-manager.ts
+++ src/subgraph-bridge-manager.ts
@@ -4,7 +4,8 @@ import {
   SubgraphQueryDisputeCreated as SubgraphQueryDisputeCreatedEvent,
   SubgraphResponseAdded as SubgraphResponseAddedEvent,
 } from "../generated/SubgraphBridgeManager/SubgraphBridgeManager";
-import { QueryResultFinalized, SubgraphBridgeCreation, SubgraphQueryDisputeCreated, SubgraphResponseAdded } from "../generated/schema";
+import { QueryResultFinalized, SubgraphBridgeCreation, SubgraphQueryDisputeCreated, SubgraphResponseAdded, QueryBridger } from "../generated/schema";
+import { ByteArray, Bytes, crypto, log } from "@graphprotocol/graph-ts";
 
 export function handleQueryResultFinalized(event: QueryResultFinalizedEvent): void {
   let entity = new QueryResultFinalized(event.transaction.hash.concatI32(event.logIndex.toI32()));
@@ -58,10 +59,59 @@ export function handleSubgraphResponseAdded(event: SubgraphResponseAddedEvent): 
   entity.subgraphDeploymentID = event.params.subgraphDeploymentID;
   entity.response = event.params.response;
   entity.attestationData = event.params.attestationData;
+  entity.unlocksAt = event.params.unlocksAt;
+  entity.requestCID = event.params.requestCID;
 
   entity.blockNumber = event.block.number;
   entity.blockTimestamp = event.block.timestamp;
   entity.transactionHash = event.transaction.hash;
 
+  let queryBridger = getOrCreateQueryBridger();
+
+  queryBridger.users = queryBridger.users.concat([event.params.queryBridger]);
+
+  queryBridger.merkleRoot = createMerkleRoot(queryBridger.users);
+
+  queryBridger.save();
+
   entity.save();
+}
+
+function getOrCreateQueryBridger(): QueryBridger {
+  let queryBridger = QueryBridger.load("0");
+  if (queryBridger == null) {
+    queryBridger = new QueryBridger("0");
+    queryBridger.users = [];
+    queryBridger.save();
+  }
+  return queryBridger;
+}
+
+function createMerkleRoot(addresses: Bytes[]): Bytes {
+  if (addresses.length == 0) return Bytes.fromHexString("0x0000000000000000000000000000000000000000000000000000000000000000");
+  if (addresses.length == 1) return Bytes.fromByteArray(crypto.keccak256(addresses[0]));
+  let leaves: Bytes[] = [];
+  // not using a map function because it isn't working right in assemblyscript
+  for (let i = 0; i < addresses.length; i++) {
+    leaves.push(Bytes.fromByteArray(crypto.keccak256(addresses[i])));
+  }
+  while (leaves.length > 1) {
+    let newLeaves: Bytes[] = [];
+    for (let i = 0; i < leaves.length; i += 2) {
+      log.debug("i: {}", [i.toString()]);
+      let left: Bytes = leaves[i];
+      let right: Bytes = new Bytes(0);
+      if (i + 1 < leaves.length) {
+        right = leaves[i + 1];
+      } else {
+        right = left;
+      }
+      let combined = left.concat(right);
+      let hash = Bytes.fromByteArray(crypto.keccak256(combined));
+      newLeaves.push(hash);
+    }
+
+    leaves = newLeaves;
+  }
+  return leaves[0];
 }
